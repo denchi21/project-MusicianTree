71dbf66d84ce2345c60b5a868954c4a04c673d4b
ivan novytskyi <vanek.rainbow86@gmail.com>
2025-06-10 10:03:32 +0200

    Оновлення секції feedback перед синхронізацією

--- src/js/basicAPI.js
+++ src/js/basicAPI.js
@@ -1,1 +1,20 @@
-// api file 
+async function fetchData(url, options = {}) {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        accept: 'application/json',
+        ...options.headers,
+      },
+      ...options,
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
+export { fetchData };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+console.log(typeof Swiper);
 
 import './js/basicAPI';
+import './js/feedback.js';
+import 'swiper/css';
+import 'css-star-rating/css/star-rating.min.css';
