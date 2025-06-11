288cc085996b3d063e1d0163eb002d4e1cad5f49
denni <dkurylenko21@gmail.com>
2025-06-11 23:22:00 +0200

    quick push

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,13 +6,6 @@ const modalContent = modalSection.querySelector('.container-modal-1');
 const closeModalBtn = modalSection.querySelector('.modal-close-btn');
 const loader = document.getElementById('modalLoader');
 
-function getBasePath() {
-  // Check if we're on GitHub Pages
-  const isGitHubPages = window.location.hostname.includes('github.io');
-  // Get the repository name from the path if on GitHub Pages
-  const repoName = isGitHubPages ? window.location.pathname.split('/')[1] : '';
-  return isGitHubPages ? `/${repoName}` : '';
-}
 
 function handleEscKey(event) {
   if (event.key === 'Escape') {
