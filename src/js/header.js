c2ce4eadd140060c1bc73799cfa839cacdfbe71d
Alina Furda <alina.furda1806@gmail.com>
2025-06-13 12:27:59 +0300

    change fix-scroll

--- src/js/header.js
+++ src/js/header.js
@@ -4,6 +4,8 @@ const closeBtn = document.querySelector('.mobile-menu-close');
 const mobileNavLinks = document.querySelectorAll('.mobile-nav-link');
 const desktopNavLinks = document.querySelectorAll('.nav-link');
 const pageHeader = document.querySelector('.page-header');
+const scrollToTopBtnDesktop = document.getElementById('scrollToTopBtnDesktop');
+const scrollToTopBtnMobile = document.getElementById('scrollToTopBtnMobile');
 
 if (burgerBtn) {
   burgerBtn.addEventListener('click', () => {
@@ -18,6 +20,24 @@ if (closeBtn) {
     document.body.style.overflow = '';
   });
 }
+
+scrollToTopBtnDesktop?.addEventListener('click', function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+scrollToTopBtnMobile?.addEventListener('click', function (e) {
+  e.preventDefault();
+  mobileMenu.classList.remove('is-open');
+  document.body.style.overflow = '';
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
 
 function smoothScroll(targetId) {
   const targetElement = document.querySelector(targetId);
@@ -51,4 +71,24 @@ mobileNavLinks.forEach(link => {
     mobileMenu.classList.remove('is-open');
     document.body.style.overflow = '';
   });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const hash = window.location.hash;
+  if (hash) {
+    const targetElement = document.querySelector(hash);
+    if (targetElement) {
+      const headerHeight = pageHeader.offsetHeight;
+      const elementPosition =
+        targetElement.getBoundingClientRect().top + window.pageYOffset;
+      const offsetPosition = elementPosition - headerHeight;
+
+      setTimeout(() => {
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'auto',
+        });
+      }, 0);
+    }
+  }
 });
